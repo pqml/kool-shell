84c826cb4d585a2bd3ed48c890f7e778f8a5c9ae
pqml <hugo.piquemal@gmail.com>
2017-02-14 03:45:23 +0100

    Fix test for windows

--- test/cleanup.js
+++ test/cleanup.js
@@ -1,14 +1,17 @@
 'use strict'
 
+const os = require('os')
 const path = require('path')
 const test = require('tape')
+
 const fork = require('./utils/forkTimeout')
 const json2t = require('./utils/json2t')
+const isWin = (os.platform() === 'win32')
 
 test(
   'cleanup plugin should be called when process exits',
   t => {
-    t.plan(3)
+    t.plan(isWin ? 2 : 3)
 
     const child1 = fork(t, path.join(__dirname, 'mocks/cleanup_exception.js'))
     child1.on('message', (m) => {
@@ -16,18 +19,6 @@ test(
       if (m.t.pass) {
         child1.clearTimeout()
         child1.kill()
-      }
-    })
-
-    const child2 = fork(t, path.join(__dirname, 'mocks/cleanup_SIGINT.js'))
-    child2.on('message', (m) => {
-      if (m.ready) child2.kill('SIGINT')
-      if (m.t) {
-        json2t(t, m.t)
-        if (m.t.pass) {
-          child2.clearTimeout()
-          child2.kill()
-        }
       }
     })
 
@@ -41,5 +32,19 @@ test(
         }
       }
     })
+
+    if (!isWin) {
+      const child2 = fork(t, path.join(__dirname, 'mocks/cleanup_SIGINT.js'))
+      child2.on('message', (m) => {
+        if (m.ready) child2.kill('SIGINT')
+        if (m.t) {
+          json2t(t, m.t)
+          if (m.t.pass) {
+            child2.clearTimeout()
+            child2.kill()
+          }
+        }
+      })
+    }
   }
 )
